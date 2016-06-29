cb4f864ee4a78888408765ba913e9e8f97fc438e
watanabe.everystudio <watanabe.everystudio@gmail.com>
2016-06-30 00:42:21 +0900

    ポケットシリーズで使ってたソースは分離

---
+++ script/RestaurantMain.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestaurantMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// このシーンはここから始まる
+		Debug.Log( "RestaurantMain.Start()" );
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
